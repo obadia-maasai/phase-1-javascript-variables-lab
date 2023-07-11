c1a578b26c2a042e87c48b5f02d5c3524993b449
obadia <obadia.maasai@student.moringaschool.com>
2023-07-11 22:29:38 +0300

    complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Code your solution in this file!
+const companyName ="Scuber"
+const CompanyName ="Scuber"
+
+let mostProfitableNeighborhood ="Chelsea";
+mostProfitableNeighborhood ="Chelsea"
+
+let companyCeo ="Susan smith";
+companyCeo ="Susan Smith"
